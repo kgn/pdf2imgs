284c496c858a236133cce0a0594084a4363d6f9c
David Keegan <git@davidkeegan.com>
2025-02-23 07:15:04 -0700

    Remove copy-webpack-plugin

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.join(__dirname, 'node_modules/pdfjs-dist/build/pdf.worker.min.js'),
-              to: path.join(__dirname, 'public/pdf.worker.min.js'),
-            },
-          ],
-        })
-      );
-    }
-    return config;
-  },
+  // Add any other Next.js config options you need here
 };
 
 module.exports = nextConfig;
